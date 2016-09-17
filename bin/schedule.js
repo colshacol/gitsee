b4a4883d851f9fe868173ade25d36eed9eb964ef
Colton Colcleasure <colshacol@gmail.com>
2016-09-16 21:56:20 -0500

    Added a blank line.

--- bin/schedule.js
+++ bin/schedule.js
@@ -13,9 +13,10 @@ const fs = require('fs');
 // for production: run every 24 hours.
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0,1,2,3,4,5,6];
-rule.hour = 5;
-rule.minute = 20;
+rule.hour = 2;
+rule.minute = 0;
 rule.second = 0;
+
 
 // for development: run every 10 seconds.
 // rule.hour = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
