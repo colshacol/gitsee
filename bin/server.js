ad237f420d90bc336ef4712e29afe99f1b080d83
Colton Colcleasure <colshacol@gmail.com>
2016-09-17 23:02:38 -0500

    Removed big ass long on every wake.

--- bin/server.js
+++ bin/server.js
@@ -48,10 +48,10 @@ function onListening() {
 
   setInterval(() => {
     http.get('http://gitsee.herokuapp.com/', (res) => {
-      console.log(res)
+      console.log('AWAKEN, MY DEMONS!')
     })
   }, 30000)
-  
+
   const date = new Date(),
     time = ((date.getHours() > 12)
       ? date.getHours() - 12
