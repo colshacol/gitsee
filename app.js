b9c95e0dd7fecab6c2524935627e008bbeb3358a
Colton Colcleasure <colshacol@gmail.com>
2016-10-29 02:31:42 -0500

    Refactored schedule.js

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const write = require('write')
+const
+  express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  write = require('write')
 
-const index = require('./routes/index')
-const repos = require('./routes/repos')
-const status = require('./routes/status')
-const users = require('./routes/users')
-const search = require('./routes/search')
+const
+  index = require('./routes/index'),
+  repos = require('./routes/repos'),
+  status = require('./routes/status'),
+  users = require('./routes/users'),
+  search = require('./routes/search')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(`public`))
+app.use(
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: false }),
+  cookieParser(),
+  express.static(`public`)
+)
 
 app.use(static)
 app.use('/', index)
@@ -28,13 +32,9 @@ app.use('/search', search)
 app.use('*', index)
 
 function static(req, res, next) {
-  if (req.url.match(/((scripts|styles|images|assets)\/\w*\.(svg|js|html|css|png|jpg|jpeg))/)) {
-    const match = req.url.match(/((scripts|styles|images|assets)\/\w*\.(svg|js|html|css|png|jpg|jpeg))/)[0]
-    console.log(`File requested: ${match}`)
-    res.sendFile(`public/${match}`, {root: './'})
-    return;
-  }
-  next()
+  const match = /((scripts|styles|images|assets)\/\w*\.(svg|js|html|css|png|jpg|jpeg))/
+  if (req.url.match(match)) return res.sendFile(`public/${req.url.match(match)[0]}`, {root: './'})
+  else next()
 }
 
-module.exports = app;
+module.exports = app
--- bin/schedule.js
+++ bin/schedule.js
@@ -1,69 +1,65 @@
-'use strict';
+'use strict'
 
-const schedule = require('node-schedule')
-const octonode = require('octonode')
-const github = octonode.client({id: 'a5a51f984' + 'c89b000260f', secret: '20e6d94a258db' + '36178f7615a574' + 'c873ef9b4a4de'})
-const mongo = require('mongojs')
-const db = mongo(`mongodb://gitsee:MarleyMC__14@ds019746.mlab.com:19746/gitsee`, ['repos'])
+const
+  schedule = require('node-schedule'),
+  octonode = require('octonode'),
+  github = octonode.client({id: 'a5a51f984' + 'c89b000260f', secret: '20e6d94a258db' + '36178f7615a574' + 'c873ef9b4a4de'}),
+  mongo = require('mongojs'),
+  db = mongo(`mongodb://gitsee:MarleyMC__14@ds019746.mlab.com:19746/gitsee`, ['repos'])
 
 // for production: run blast every 24 hours.
-const rule = new schedule.RecurrenceRule()
-rule.dayOfWeek = [0,1,2,3,4,5,6]
-rule.hour = 18;
-rule.minute = 13;
-rule.second = 50;
+const
+  rule = new schedule.RecurrenceRule()
+  rule.dayOfWeek = [0,1,2,3,4,5,6]
+  rule.hour = 18
+  rule.minute = 13
+  rule.second = 50
 
 const blast = () => {
   // Create the function that will fire at the specified time.
   schedule.scheduleJob(rule, () => {
-    console.log('\n\n\n\nblasting\n\n\n\n');
+    console.log('\n\n\n\nblasting\n\n\n\n')
 
     // Find all repos in the database.
     db.repos.find((error, repos) => {
-      console.log(`\n\n\n${repos.length} repos updating.\n\n\n`);
       // For each repo found found...
       for (let i = 0; i < repos.length; i++) {
-        let repo = repos[i];
-        let fullname = repo.repo;
-        let history = repo.history;
-
-        console.log(`\n${fullname} history updating...`);
+        let repo = repos[i]
+        let fullname = repo.repo
+        let history = repo.history
 
         const
-          date = new Date(),
-          day = date.getDate(),
-          month = date.getMonth() + 1,
-          year = date.getFullYear(),
-          nowDate = `${month}/${day}/${year}`,
+          nowDate = new Date(),
+          day = nowDate.getDate(),
+          month = nowDate.getMonth() + 1,
+          year = nowDate.getFullYear(),
+          date = `${month}/${day}/${year}`,
           time = Date.now()
 
         // Query GitHub to get current repo stats.
         github.get(`/repos/${fullname}`, (err, status, body, headers) => {
-          if (err) {
-            console.log('Error querying GitHub.');
-            return;
-          };
+          if (err) return console.log('Error querying GitHub.')
 
           history.push({
-            date: nowDate,
-            time: time,
+            date,
+            time,
             stars: body.stargazers_count,
             watchers: body.subscribers_count,
             forks: body.forks_count,
             issues: body.open_issues_count
-          });
+          })
 
           // Find the repo at hand and update the history array.
           db.repos.findAndModify({
             query: { repo: fullname },
-            update: { $set: { history: history } }
+            update: { $set: { history } }
           }, (err, doc, lastErrorObject) => {
-            console.log(`\n\n${fullname} history updated.\n\n`);
+            console.log(`\n\n${fullname} history updated.\n\n`)
           })
-        });
+        })
       }
-    });
-  });
+    })
+  })
 }
 
-module.exports = blast;
+module.exports = blast
